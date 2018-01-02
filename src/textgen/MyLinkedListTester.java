4a6faada0405fd5ad8b212700748bbb4efb8ab1a
jbouhier <jb.bouhier@gmail.com>
2018-01-01 20:37:53 -0800

    MyLinkedListTester: Fixed tests - Grader 13/13 OK

--- src/textgen/MyLinkedListTester.java
+++ src/textgen/MyLinkedListTester.java
@@ -15,6 +15,7 @@ public class MyLinkedListTester {
 	MyLinkedList<Integer> longerList;
 	MyLinkedList<Integer> list1;
 
+
 	/** Run before each @Test method */
 	@Before
 	public void setUp() {
@@ -34,53 +35,113 @@ public class MyLinkedListTester {
 		}
 	}
 
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void emptyListGet() {
-		emptyList.get(0);
-		emptyList.get(-1);
-	}
-
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void shortListGetOutBounds() {
-		shortList.get(-1);
-		shortList.get(2);
-	}
-
 	@Test
-	public void shortListGet() {
+	public void testGet() {
+		// Empty list bounds
+		try {
+			emptyList.get(0);
+			fail("Check out of bounds");
+		}
+		catch (IndexOutOfBoundsException e) {}
+
+		// Short list, first contents, then bounds
 		assertEquals("Check first", "A", shortList.get(0));
 		assertEquals("Check second", "B", shortList.get(1));
-	}
 
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void longerListGet() {
+		try {
+			shortList.get(-1);
+			fail("Check out of bounds");
+		}
+		catch (IndexOutOfBoundsException e) {}
+
+		try {
+			shortList.get(2);
+			fail("Check out of bounds");
+		}
+		catch (IndexOutOfBoundsException e) {}
+
+		// Longer list contents
 		for (int i = 0; i < LONG_LIST_LENGTH; i++) {
-			assertEquals("Check " + i + " element", (Integer)i, longerList.get(i));
+			assertEquals("Check "+ i + " element", (Integer)i, longerList.get(i));
 		}
 
-		longerList.get(-1);
-		longerList.get(LONG_LIST_LENGTH);
-	}
+		// Longer list bounds
+		try {
+			longerList.get(-1);
+			fail("Check out of bounds");
+		}
+		catch (IndexOutOfBoundsException e) {}
 
-	/** Test removing an element from the list.
-	 * We've included the example from the concept challenge.
-	 * You will want to add more tests.  */
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void list1RemoveOutBounds() {
-		list1.remove(42);
-		list1.remove(-2);
+		try {
+			longerList.get(LONG_LIST_LENGTH);
+			fail("Check out of bounds");
+		}
+		catch (IndexOutOfBoundsException e) {}
 	}
 
 	@Test
-	public void list1Remove() {
+	public void testSet() {
+		// Test null and bounds
+		try {
+			list1.set(0,  null);
+			fail("Check if 2nd param is null");
+		} catch (NullPointerException e) {}
+
+		try {
+			list1.set(-1, 99);
+			fail("Check out of bounds");
+		} catch (IndexOutOfBoundsException e) {}
+
+		try {
+			list1.set(list1.size(), 99);
+			fail("Check out of bounds");
+		} catch (IndexOutOfBoundsException e) {}
+
+		// Test valid change
+		assertEquals("TEST: list1[0] = 65", (Integer)65, list1.get(0));
+		list1.set(0, 42);
+		assertEquals("TEST: list1[0] = 42", (Integer)42, list1.get(0));
+	}
+
+	@Test
+	public void testRemove() {
+		try {
+			list1.remove(42);
+			fail("Check out of bounds");
+		} catch (IndexOutOfBoundsException e) {}
+
+		try {
+			list1.remove(-2);
+			fail("Check out of bounds");
+		} catch (IndexOutOfBoundsException e) {}
+
 		assertEquals("Remove: check a is correct ", (Integer)65, list1.remove(0));
 		assertEquals("Remove: check element 0 is correct ", (Integer)21, list1.get(0));
 		assertEquals("Remove: check size is correct ", 2, list1.size());
 	}
 
-	@Test(expected = NullPointerException.class)
-	public void shortListAdd() {
-		shortList.add(null);
+	@Test
+	public void testAdd() {
+		try {
+			shortList.add(null);
+			fail("Check null paramater");
+		} catch (NullPointerException e) {}
+
+		try {
+			shortList.add(1, null);
+			fail("Check null paramater");
+		} catch (NullPointerException e) {}
+
+		try {
+			shortList.add(42, "X");
+			fail("Check out of bounds");
+		} catch (IndexOutOfBoundsException e) {}
+
+		try {
+			shortList.add(-21, "Z");
+			fail("Check out of bounds");
+		} catch (IndexOutOfBoundsException e) {}
+
 		assertEquals("Adding 'null' shouldn't change LinkedList", 2, shortList.size());
 		assertEquals("Index 1 should equal to the value 'B'", "B", shortList.get(1));
 		shortList.add("Plop");
@@ -88,66 +149,4 @@ public class MyLinkedListTester {
 		assertEquals("Index 2 should equal to the value 'Plop'", "Plop", shortList.get(2));
 	}
 
-	@Test(expected = NullPointerException.class)
-	public void shortListAddAtIndexNullObj() {
-		shortList.add(1, null);
-		shortList.add(42, "X");
-		shortList.add(-21, "Z");
-		assertEquals("shortList size is 0", 2, shortList.size());
-		shortList.add(0, "Meh");
-		assertEquals("shortList size is 3", 3, shortList.size());
-		assertEquals("index 0 of shortList is 'Meh'", "Meh", shortList.get(0));
-	}
-
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void shortListAddAtIndexOutBounds() {
-		shortList.add(42, "X");
-		shortList.add(-21, "Z");
-		assertEquals("shortList size is 0", 2, shortList.size());
-	}
-
-	@Test
-	public void shortListAddAtIndex() {
-		shortList.add(0, "Meh");
-		assertEquals("shortList size is 3", 3, shortList.size());
-		assertEquals("index 0 of shortList is 'Meh'", "Meh", shortList.get(0));
-	}
-
-	@Test
-	public void testSize() {
-		assertEquals("emptyList.size = 0", (Integer)0, (Integer)emptyList.size());
-		assertEquals("shortList.size = 2", (Integer)2, (Integer)shortList.size());
-		assertEquals("list1.size = 3", (Integer)3, (Integer)list1.size());
-		assertEquals("longerList.size = 3", (Integer)LONG_LIST_LENGTH, (Integer)longerList.size());
-		// Size after add
-		// Size after remove
-	}
-
-	@Test(expected = NullPointerException.class)
-	public void list1SetNullObj() {
-		list1.set(0,  null);
-	}
-
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void list1SetOutBounds() {
-		list1.set(-2, -2);
-		list1.set(55,  55);
-	}
-
-	@Test
-	public void list1Set() {
-		assertEquals("TEST: list1[0] = 65", (Integer)65, list1.get(0));
-		list1.set(0, 42);
-		assertEquals("TEST: list1[0] = 42", (Integer)42, list1.get(0));
-	}
-
-	@Test
-	public void testToString() {
-		assertTrue("TEST: shortList.toString", emptyList.toString().equals(""));
-//		assertEquals("TEST: emptyList.toString = \"\"", "", emptyList.toString());
-//		assertEquals("TEST: shortList.toString", "A, B", shortList.toString());
-//		assertEquals("TEST: list1.toString", "65, 21, 42", list1.toString());
-//		assertEquals("TEST: longerList.toString", "0, 1, 2, 3, 4, 5, 6, 7, 8, 9", longerList.toString());
-	}
-	
 }
