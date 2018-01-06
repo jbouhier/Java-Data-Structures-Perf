43dd5ee08926f03d2b491e5fa5a08029767d4fa7
jbouhier <jb.bouhier@gmail.com>
2018-01-06 13:15:28 -0800

    DictionaryLL: Implementation OK - Tests 100% green

--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -6,37 +6,38 @@ import java.util.LinkedList;
  * A class that implements the Dictionary interface using a LinkedList
  *
  */
-public class DictionaryLL implements Dictionary 
-{
+public class DictionaryLL implements Dictionary {
 
 	private LinkedList<String> dict;
-	
-    // TODO: Add a constructor
+
+    public DictionaryLL() {
+        dict = new LinkedList<>();
+    }
 
 
-    /** Add this word to the dictionary.  Convert it to lowercase first
-     * for the assignment requirements.
+    /**
+     * Add a word to the dictionary. (it wasn't already there).
+     * Convert it to lowercase first.
+     *
      * @param word The word to add
      * @return true if the word was added to the dictionary 
-     * (it wasn't already there). */
+     **/
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+        if (isWord(word)) return false;
+        return dict.add(word.toLowerCase());
     }
 
 
     /** Return the number of words in the dictionary */
-    public int size()
-    {
-        // TODO: Implement this method
-        return 0;
+    public int size() {
+        return dict.size();
     }
+
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-        //TODO: Implement this method
-        return false;
+        return dict.contains(s.toLowerCase());
     }
 
-    
+
 }
