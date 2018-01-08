9908b59145a7f8a7ae47fb5d8a16445183eb504c
jbouhier <jb.bouhier@gmail.com>
2018-01-07 16:46:52 -0800

    TrieNode: Removed excess lines / spaces

--- src/spelling/TrieNode.java
+++ src/spelling/TrieNode.java
@@ -9,21 +9,19 @@ import java.util.Set;
  *
  */
 class TrieNode {
-	private HashMap<Character, TrieNode> children; 
+	private HashMap<Character, TrieNode> children;
 	private String text;  // Maybe omit for space
 	private boolean isWord;
 	
 	/** Create a new TrieNode */
-	public TrieNode()
-	{
-		children = new HashMap<Character, TrieNode>();
+	public TrieNode() {
+		children = new HashMap<>();
 		text = "";
 		isWord = false;
 	}
 	
 	/** Create a new TrieNode given a text String to store in it */
-	public TrieNode(String text)
-	{
+	public TrieNode(String text) {
 		this();
 		this.text = text;
 	}
@@ -47,11 +45,9 @@ class TrieNode {
 	 * @return The newly created TrieNode, or null if the node is already 
 	 *     in the trie.
 	 */
-	public TrieNode insert(Character c)
-	{
-		if (children.containsKey(c)) {
+	public TrieNode insert(Character c) {
+		if (children.containsKey(c))
 			return null;
-		}
 		
 		TrieNode next = new TrieNode(text + c.toString());
 		children.put(c, next);
@@ -59,26 +55,22 @@ class TrieNode {
 	}
 	
 	/** Return the text string at this node */
-    public String getText()
-	{
+    public String getText() {
 		return text;
 	}
 	
     /** Set whether or not this node ends a word in the trie. */
-	public void setEndsWord(boolean b)
-	{
+	public void setEndsWord(boolean b) {
 		isWord = b;
 	}
 	
 	/** Return whether or not this node ends a word in the trie. */
-	public boolean endsWord()
-	{
+	public boolean endsWord() {
 		return isWord;
 	}
 	
 	/** Return the set of characters that have links from this node */
-	public Set<Character> getValidNextCharacters()
-	{
+	public Set<Character> getValidNextCharacters() {
 		return children.keySet();
 	}
 
