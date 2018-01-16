2f36dae107fdcee954decb1f5b9281e877457c50
jbouhier <jb.bouhier@gmail.com>
2018-01-16 10:34:53 -0500

    NerbyWordsTester: Create unit test file for class NerbyWords TDD

---
+++ src/spelling/NerbyWordsTester.java
@@ -0,0 +1,24 @@
+package spelling;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Jean-Baptiste Bouhier
+ */
+public class NerbyWordsTester {
+
+	@Before
+	public void setup() {
+
+	}
+
+	@Test
+	public void testOne() {
+		assertEquals("Test 1: ", 1, 1);
+	}
+
+
+}
