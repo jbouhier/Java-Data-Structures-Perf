70025ad651e7e89df2a2d8eb30ca8fffcea8c4f2
jbouhier <jb.bouhier@gmail.com>
2018-02-17 00:22:35 -0800

    NerbyWordsTester: Added test for deletions() w/ & w/o dictionary words only

--- src/spelling/NerbyWordsTester.java
+++ src/spelling/NerbyWordsTester.java
@@ -1,23 +1,50 @@
 package spelling;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 /**
  * @author Jean-Baptiste Bouhier
  */
 public class NerbyWordsTester {
 
+	private Dictionary d;
+	private NearbyWords nw;
+	private List<String> list;
+
 	@Before
 	public void setup() {
-
+		d = new DictionaryHashSet();
+		DictionaryLoader.loadDictionary(d, "data/dict.txt");
+		nw = new NearbyWords(d);
+		list = new ArrayList<>();
 	}
 
 	@Test
-	public void testOne() {
-		assertEquals("Test 1: ", 1, 1);
+	public void deletionsIsWord() {
+		List<String> control = new ArrayList<>(Arrays.asList("horse", "horse"));
+		String word = "horsee";
+		nw.deletions(word, list, true);
+		Assert.assertEquals(control, list);
+	}
+
+	@Test
+	public void deletionsNotWord() {
+		List<String> control = Stream
+				.of("orsee", "hrsee", "hosee", "horee", "horse", "horse")
+				.collect(Collectors.toCollection(ArrayList::new));
+
+		String word = "horsee";
+		nw.deletions(word, list, false);
+		Assert.assertEquals(control, list);
 	}
 
 
